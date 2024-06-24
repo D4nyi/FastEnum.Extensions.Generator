bf8f91b694659debc5c91a2572cd717cd128d548
Szöllősi Dániel <szollosi.daniel0@gmail.com>
2024-06-24 18:01:58 +0200

    Snapshot tests updated

--- test/FastEnum.Extensions.Generator.Tests/Snapshots/net8/EnumToStringGeneratorTests.RunResults#ColorExtensions.g.verified.cs
+++ test/FastEnum.Extensions.Generator.Tests/Snapshots/net8/EnumToStringGeneratorTests.RunResults#ColorExtensions.g.verified.cs
@@ -9,7 +9,7 @@ namespace SnapshotTesting;
 /// <summary>
 /// Extension methods for <see cref="SnapshotTesting.Color" />
 /// </summary>
-[global::System.CodeDom.Compiler.GeneratedCode("FastEnum.Helpers.Generator.EnumToStringGenerator", "1.0.0-preview.4")]
+[global::System.CodeDom.Compiler.GeneratedCode("FastEnum.Helpers.Generator.EnumToStringGenerator", "1.0.0")]
 public static class ColorExtensions
 {
     private static readonly global::System.Int32[] _underlyingValues = 
