52e977a5cbbdf68e739d94aa68a469c79fb7246a
Szöllősi Dániel <szollosi.daniel0@gmail.com>
2025-01-01 13:59:24 +0100

    Reset

--- test/FastEnum.Extensions.Generator.Tests.Snapshot/EnumToStringGeneratorTests.cs
+++ test/FastEnum.Extensions.Generator.Tests.Snapshot/EnumToStringGeneratorTests.cs
@@ -1,32 +1,12 @@
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
-using System.Runtime.Serialization;
-using System.Text;
-
 using FastEnum.Extensions.Generator.Tests.Snapshot.Helpers;
 
 using Microsoft.CodeAnalysis;
-
-using Xunit.Abstractions;
 
 namespace FastEnum.Extensions.Generator.Tests.Snapshot;
 
 public sealed class EnumToStringGeneratorTests
 {
     [Fact]
-    public async Task Log()
-    {
-        await using FileStream fs = File.OpenWrite(
-            "/home/runner/work/FastEnum.Extensions.Generator/FastEnum.Extensions.Generator/test/" +
-            $"FastEnum.Extensions.Generator.Tests.Snapshot/bin/Debug/output-{Random.Shared.Next()}.txt");
-
-        await fs.WriteAsync(Encoding.UTF8.GetBytes(typeof(object).Assembly.Location));
-        await fs.WriteAsync(Encoding.UTF8.GetBytes(typeof(DescriptionAttribute).Assembly.Location));
-        await fs.WriteAsync(Encoding.UTF8.GetBytes(typeof(EnumMemberAttribute).Assembly.Location));
-        await fs.WriteAsync(Encoding.UTF8.GetBytes(typeof(DisplayAttribute).Assembly.Location));
-    }
-
-    // [Fact]
     public Task GeneratesEnumExtensionsCorrectly()
     {
         // Arrange
