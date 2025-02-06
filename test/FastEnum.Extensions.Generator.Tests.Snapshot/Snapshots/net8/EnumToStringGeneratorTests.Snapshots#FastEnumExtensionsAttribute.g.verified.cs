e4e1c294415e708613ebebf8c6f253132282b1ee
Szöllősi Dániel <szollosi.daniel0@gmail.com>
2025-02-06 18:32:03 +0100

    Removed `ConditionalAttribute`

--- test/FastEnum.Extensions.Generator.Tests.Snapshot/Snapshots/net8/EnumToStringGeneratorTests.Snapshots#FastEnumExtensionsAttribute.g.verified.cs
+++ test/FastEnum.Extensions.Generator.Tests.Snapshot/Snapshots/net8/EnumToStringGeneratorTests.Snapshots#FastEnumExtensionsAttribute.g.verified.cs
@@ -5,7 +5,6 @@ namespace FastEnum
 {
     /// <summary>Marks an enum to generate optimized extensions for it.</summary>
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("FastEnum.Extensions.Generator.EnumExtensionsGenerator", "1.4.0")]
-    [global::System.Diagnostics.ConditionalAttribute("DEBUG")]
     [global::System.AttributeUsageAttribute(global::System.AttributeTargets.Enum, AllowMultiple = false, Inherited = false)]
     internal sealed class ExtensionsAttribute : global::System.Attribute { }
 }
