39244c0a6265ed94972796695c9817426308cd3a
Dani <szollosi.daniel0@gmail.com>
2026-02-23 17:48:00 +0100

    Change workflows

--- src/FastEnum.Extensions.Generator/Properties/Assembly.cs
+++ src/FastEnum.Extensions.Generator/Properties/Assembly.cs
@@ -4,10 +4,6 @@ using System.Reflection;
 
 [assembly: CLSCompliant(false)]
 
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("<Version> Built: <BuildTime>")]
-
 [assembly: AssemblyProduct("FastEnum.Extensions.Generator")]
 [assembly: AssemblyTitle("FastEnum.Extensions.Generator")]
-[assembly: AssemblyCopyright("Copyright © <ReleaseYear>. Dániel Szöllősi")]
 [assembly: AssemblyDescription("Source generates extension methods for enums to work with them easier and faster.")]
